54c9f073c43a39139800f282d269d3346ac8c41b
NICHOLAS CONTIJO MOREIRA <68407414+Nick91-qui@users.noreply.github.com>
2025-08-27 18:19:16 -0300

    refactor(schemas): update pydantic config from orm_mode to from_attributes
    This change aligns with Pydantic v2's breaking change where `orm_mode` was renamed to `from_attributes`

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -14,7 +14,7 @@ class User(UserBase):
     is_active: bool
 
     class Config:
-        orm_mode = True
+        from_attributes = True # Alterado de orm_mode = True
 
 class Token(BaseModel):
     access_token: str
